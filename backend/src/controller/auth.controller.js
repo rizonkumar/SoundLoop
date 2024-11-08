b9e3414ce3bc2cb27aa61293eece3235bfc67878
Rizon Rahi <rizon.kumar.rahi@gmail.com>
2024-11-08 07:04:21 +0530

    Feat: Created Contoller for Auth

---
+++ backend/src/controller/auth.controller.js
@@ -0,0 +1,27 @@
+import { User } from "../models/user.model.js";
+
+export const authCallBack = async (req, res) => {
+  try {
+    const { id, firstName, lastName, imageUrl } = req.body;
+
+    // check if user exists in our database
+    const user = await User.findOne({ clerkId: id });
+    if (!user) {
+      //signup the user
+      await User.create({
+        clerkId: id,
+        fullName: `${firstName} ${lastName}`,
+        imageUrl,
+      });
+    }
+    res
+      .status(200)
+      .json({ message: "User authenticated successfully", success: true });
+  } catch (error) {
+    res.status(500).json({
+      message: "Internal server error",
+      error: error.message,
+      success: false,
+    });
+  }
+};
