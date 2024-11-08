2f637cb6bd679e5f7a2e82dfbc79e1c06bf1decf
Rizon Rahi <rizon.kumar.rahi@gmail.com>
2024-11-08 09:31:02 +0530

    Feat: Implemented Album create and delete with better comment

--- backend/src/routes/admin.routes.js
+++ backend/src/routes/admin.routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
+
 import { protectRoute, requireAdmin } from "../middleware/auth.middleware.js";
 import {
   createAlbum,
   createSong,
   deleteSong,
+  deleteAlbum,
 } from "../controller/admin.controller.js";
 
 const router = Router();
@@ -12,5 +14,6 @@ router.post("/songs", protectRoute, requireAdmin, createSong);
 router.delete("/songs/:id", protectRoute, requireAdmin, deleteSong);
 
 router.post("/album", protectRoute, requireAdmin, createAlbum);
+router.delete("/album/:id", protectRoute, requireAdmin, deleteAlbum);
 
 export default router;
