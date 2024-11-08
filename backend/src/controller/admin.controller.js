1e0246fcbee341b629ee47fbe89bb247d8c6ac5a
Rizon Rahi <rizon.kumar.rahi@gmail.com>
2024-11-08 09:24:15 +0530

    Feat: Implemented Delelte Song Controller and Route

--- backend/src/controller/admin.controller.js
+++ backend/src/controller/admin.controller.js
@@ -37,3 +37,26 @@ export const createSong = async (req, res, next) => {
     next(error);
   }
 };
+
+export const deleteSong = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const song = await Song.findById(id);
+
+    // if song belongs to an album, remove it from the album's songs array and update the album
+    if (song.albumId) {
+      await Album.findByIdAndUpdate(song.albumId, {
+        $pull: { songs: song._id },
+      });
+      await Song.findByIdAndDelete(id);
+      res
+        .status(200)
+        .json({ message: "Song deleted successfully.", success: true });
+    }
+  } catch (error) {
+    console.log("error in deleteSong controller", error);
+    next(error);
+  }
+};
+
+export const createAlbum = async (req, res, next) => {};
--- backend/src/routes/admin.routes.js
+++ backend/src/routes/admin.routes.js
@@ -1,9 +1,16 @@
 import { Router } from "express";
 import { protectRoute, requireAdmin } from "../middleware/auth.middleware.js";
-import { createSong } from "../controller/admin.controller.js";
+import {
+  createAlbum,
+  createSong,
+  deleteSong,
+} from "../controller/admin.controller.js";
 
 const router = Router();
 
 router.post("/songs", protectRoute, requireAdmin, createSong);
+router.delete("/songs/:id", protectRoute, requireAdmin, deleteSong);
+
+router.post("/album", protectRoute, requireAdmin, createAlbum);
 
 export default router;
