47a376130dfc534072990a4a444a3126567965c8
Rizon Rahi <rizon.kumar.rahi@gmail.com>
2024-11-08 09:33:26 +0530

    Feat: Added Check Admin Controller and Route

--- backend/src/controller/admin.controller.js
+++ backend/src/controller/admin.controller.js
@@ -118,3 +118,9 @@ export const deleteAlbum = async (req, res, next) => {
     next(error);
   }
 };
+
+export const checkAdmin = async (req, res, next) => {
+  res
+    .status(200)
+    .json({ message: "You are an admin.", success: true, admin: true });
+};
--- backend/src/routes/admin.routes.js
+++ backend/src/routes/admin.routes.js
@@ -6,10 +6,12 @@ import {
   createSong,
   deleteSong,
   deleteAlbum,
+  checkAdmin,
 } from "../controller/admin.controller.js";
 
 const router = Router();
 
+router.get("/check", protectRoute, requireAdmin, checkAdmin);
 router.post("/songs", protectRoute, requireAdmin, createSong);
 router.delete("/songs/:id", protectRoute, requireAdmin, deleteSong);
 
